edbc6f8cf86a91a0ab2c5d0ddf10901d72d64b6b
Marcos Zeballos <marcoszeballos8@gmail.com>
2023-10-25 22:34:50 -0300

    2da clase de javascript

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log('mi primer proyecto');
+"use strict";
 
+let num1 = Number(prompt("Ingrese Numero: "));
 
+if (num1 > 1000){
+    alert("El numero es mayor a 1000");
+
+} 
+else{
+    alert("El numero es menor a 1000");
+}
 
